8c697be9ffd857432329d8b0eb3091e6e90f2ba2
dashakrolik <45757824+dashakrolik@users.noreply.github.com>
2020-01-23 14:24:50 +0100

    added a separate DTO for waveHeight with getters and setters however object mapped to entity from external api is still a string...

---
+++ src/main/java/com/ordina/surfforecast/model/WaveHeightDTO.java
@@ -0,0 +1,14 @@
+package com.ordina.surfforecast.model;
+
+public class WaveHeightDTO {
+    String hours;
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+}
--- src/main/java/com/ordina/surfforecast/service/BeachService.java
+++ src/main/java/com/ordina/surfforecast/service/BeachService.java
@@ -1,5 +1,6 @@
 package com.ordina.surfforecast.service;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -7,6 +8,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ordina.surfforecast.model.WaveHeightDTO;
 import com.ordina.surfforecast.repository.BeachRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +27,8 @@ import com.ordina.surfforecast.model.BeachEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.transform.Result;
+
 @Service
 public class BeachService {
      
@@ -35,8 +42,10 @@ public class BeachService {
         if(beachesList.size() > 0) {
             List<BeachEntity> arrayToLoop = beachesList.stream().map(beach -> {
                 try {
-                    String result = getForecast(beach);
-                    beach.setWaveHeight(result);
+                    String response = getForecast(beach);
+//                    String value = result.hours[0].waveHeight[0].value;
+
+                    beach.setWaveHeight(response);
                     return beach;
                 } catch (URISyntaxException e) {
                     e.printStackTrace();
@@ -55,12 +64,12 @@ public class BeachService {
     public String getForecast(BeachEntity beach) throws URISyntaxException {
         HttpHeaders requestHeaders = new HttpHeaders();
         requestHeaders.set("Authorization", "1f1a92e0-36d1-11ea-83df-0242ac130002-1f1a93f8-36d1-11ea-83df-0242ac130002");
+
         HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
 
         RestTemplate restTemplate = new RestTemplate();
 
         restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-
 
         try {
             // Make the HTTP GET request to the Basic Auth protected URL
@@ -68,8 +77,8 @@ public class BeachService {
             String lng = beach.getLongitude();
             LocalDate localDate = LocalDate.now();
             ResponseEntity<String> response = restTemplate.exchange("https://api.stormglass.io/v1/weather/point?params=waveHeight&source=dwd" + "&start=" + localDate + "&end=" + localDate + "&lat=" + lat + "&lng=" + lng, HttpMethod.GET, requestEntity, String.class);
+//            System.out.println(response.getBody());
 
-            System.out.println(response.getBody());
             return response.getBody();
         } catch (HttpClientErrorException e) {
             return e.toString();
